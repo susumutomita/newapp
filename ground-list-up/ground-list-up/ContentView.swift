9951421dec000fd674b482c6d1f8d64d17e6ec59
susumu tomita <oyster880@gmail.com>
2022-12-05 08:33:05 +0900

    Add Title

--- ground-list-up/ground-list-up/ContentView.swift
+++ ground-list-up/ground-list-up/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
-                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/ } .navigationBarTitle("Navigation Bar")
+                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) { /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/ } .navigationBarTitle("Ground List")
             }
         }
         .padding()
