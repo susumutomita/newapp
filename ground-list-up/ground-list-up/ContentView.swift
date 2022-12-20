ac341515a7835f45574e1dac457bf675a79a8572
susumu tomita <oyster880@gmail.com>
2022-12-20 10:13:13 +0900

    animation

--- ground-list-up/ground-list-up/ContentView.swift
+++ ground-list-up/ground-list-up/ContentView.swift
@@ -25,20 +25,41 @@ struct ContentView: View {
     @State var isJapanese = true
     var body: some View
     {
-        VStack{
-            Text(isJapanese ? japanese : cards[japanese]!)
-            HStack
-            {
-                Button(action:{
-                    self.isJapanese.toggle()
-                }){
-                Text("裏返す")
+        ZStack {
+            Color(red: 0.97, green: 0.96, blue:0.56).edgesIgnoringSafeArea(.all)
+            VStack{
+                ZStack{
+                    Image("card")
+                        .resizable()
+                        .frame(width: 300.0,height:133.0)
+                        .shadow(radius: 3)
+                        .rotation3DEffect(.degrees(isJapanese ? 0 : 180), axis: (x: 0, y: 1, z: 0))
+                        .animation(.spring())
+                        .onTapGesture {
+                            self.isJapanese.toggle()
+                        }
+                    Text(isJapanese ? japanese : cards[japanese]!).font(.largeTitle)
                 }
-                Button(action: {
-                    self.isJapanese = true
-                    self.japanese = self.cards.randomElement()!.key
-                }){
-                    Text("次へ")
+                HStack
+                {
+                    Group{
+                        Button(action:{
+                            self.isJapanese.toggle()
+                        }){
+                            Image(systemName: "arrow.2.circlepath")
+                            Text("裏返す")
+                        }
+                        Button(action: {
+                            self.isJapanese = true
+                            self.japanese = self.cards.randomElement()!.key
+                        }){
+                            Image(systemName: "forward.fill")
+                            Text("次へ")
+                        }
+                    }.padding()
+                        .foregroundColor(.white)
+                        .background(Color(red: 0.86, green:0.45,blue:0.03))
+                        .cornerRadius(10)
                 }
             }
         }
